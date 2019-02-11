33e194a80d083193a1294c0bf488d8532b0140b3
o.shumelyuk <o.shumelyuk@gmail.com>
2019-02-11 17:38:02 +0200

    fix Hello World mail to send run as aws lambda

--- AwsHelloWorldMail/WordsWriter.cs
+++ AwsHelloWorldMail/WordsWriter.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Amazon;
+using Amazon.Lambda.Core;
 using Amazon.Runtime.Internal.Util;
 using Amazon.SimpleEmail;
 using Amazon.SimpleEmail.Model;
 
 namespace AwsHelloWorldMail
 {
-    public static class WordsWriter
+    public class WordsWriter
     {
-        public static void SayHello()
+        [LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+        public async Task<string> SayHello()
         {
             try
             {
@@ -20,19 +23,19 @@ namespace AwsHelloWorldMail
                 var source = "o.shumeliuk@levi9.com";
                 var destination = new Destination(new List<string> { "o.shumeliuk@levi9.com" });
                 var message = new Message(new Content("Hello from AWS Lambda"), new Body(new Content("Body printed by AWS Lambda: Hello world")));
-                var resp = client.SendEmailAsync(new SendEmailRequest(source, destination, message))
-                    .GetAwaiter().GetResult();
+                var resp = await client.SendEmailAsync(new SendEmailRequest(source, destination, message));
                 if (resp.HttpStatusCode != System.Net.HttpStatusCode.OK)
                 {
                     throw new ArgumentException($"Http code expected to be OK but found {resp.HttpStatusCode.ToString()}. Response MessageId: {resp.MessageId}");
                 }
+                return "Hello World!";
             }
             catch (Exception e)
             {
-                Logger.GetLogger(typeof(WordsWriter)).Error(e, "ERROR!");
+                LambdaLogger.Log("SayHello throws and error" + e.Message);
+                //Logger.GetLogger(typeof(WordsWriter)).Error(e, "ERROR!");
                 throw;
             }
-
         }
     }
 }
